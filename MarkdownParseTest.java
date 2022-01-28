b0740b87bddf263993183f24d635e6aad16f67de
DarrenNg <ngwdarren@gmail.com>
2022-01-27 16:34:07 -0800

    Added 2 more tests for more files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -3,6 +3,8 @@ import org.junit.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 //javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
 
@@ -13,9 +15,24 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
     @Test
     public void testFile() throws IOException{
-        assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(Files.readString("test-file.md")));
+
+        assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
     }
+
+    @Test
+    public void test2() throws IOException{
+
+        assertEquals(List.of("link1.html"), MarkdownParse.getLinks(Files.readString(Path.of("test2.md"))));
+    }
+
+    @Test
+    public void test3() throws IOException{
+
+        assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("test3.md"))));
+    }
+
 
 }
